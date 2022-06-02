effc7c62b7d4c09e724e239ec220681789cd9226
Bbnazarkul <bbnazarkul@gmail.com>
2022-06-02 18:27:23 +0600

    feat: add H_W

---
+++ 31-05/script.js
@@ -0,0 +1,45 @@
+/*********** Создайте объект заработных плат.
+ Назовите userSalaries.
+ Свойствами выступают: Ella, Sophia, Ellie
+ Выведите на экран зарплату Ella, Sophia.***********/
+/*const userSalaries = {
+  Ella: 100,
+  Sophia: 200,
+  Ellie: 300,
+};
+alert(userSalaries['Ella'] + userSalaries['Sophia'] + userSalaries['Ellie']) ;*/
+
+
+
+/*********** Создайте объект с ключами a, b и c значениями 5 , 6 и 10.
+ Найдите сумму его элементов ***********/
+
+/*const obj = {
+  a: 5,
+  b: 6,
+  c: 10,
+}
+console.log(obj['a'] + obj['b'] + obj['c'])*/
+
+
+/*********** let number;
+ if(data === true){
+     number = 3;
+  } else{
+     number = 5;
+  }
+
+ Сделайте рефакторинг кода, с помощью тернарного оператора. ***********/
+
+let number;
+let data = true;
+if(data === true){
+  number = 3;
+} else{
+  number = 5;
+}
+
+alert(data);
+
+/*result  (number === 15) ? '3' : '5';*/
+
