dfb3672ce7be1e30323724f6bf3995601e30ccff
Bbnazarkul <bbnazarkul@gmail.com>
2022-06-14 18:32:51 +0600

    feat: add H_w

---
+++ 11-06/script.js
@@ -0,0 +1,25 @@
+function min(a, b) {
+  if (a < b) {
+    console.log(a);
+  } else {
+    console.log(b);
+  }
+}
+min();
+
+
+function num (c) {
+  console.log(c)
+}
+
+num();
+
+
+
+function main(){
+  min(5, 10);
+  num(15);
+}
+
+main();
+
