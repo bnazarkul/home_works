0f47852c275756ef3fc39e3923d5d20e91a1bcc0
Bbnazarkul <bbnazarkul@gmail.com>
2022-06-23 18:34:22 +0600

    feat: add HomeWork

--- 11-06/script.js
+++ 11-06/script.js
@@ -1,3 +1,4 @@
+/*
 function min(a, b) {
   if (a < b) {
     console.log(a);
@@ -23,3 +24,33 @@ function main(){
 
 main();
 
+*/
+
+
+function myFunc (arr, number) {
+  const newArr = [];
+  for (let i = 0; i < myFunc; i++) {
+    newArr.push(arr, number);
+  }
+}
+
+myFunc('a', 3);
+
+
+/*function example(exam, projects) {
+  if (exam > 90 || projects >10) { return 100;}
+  else if (exam > 75 && projects > 5 ) { return 90;}
+  else if (exam > 50 && projects > 2 ) { return 75;}
+  else if (exam > 30 && projects > 1 ) { return 50;}
+  else { return 0;}
+}
+console.log(example(100, 9));*/
+
+
+
+
+/*
+заполннить массив цикл (фор ай) и метод пуш (массивы данных)
+
+условные конструкции и
+логические оператор и или*/
